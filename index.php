a45c533c970a7c94c8bf0e8cb3ec4695058ac4b8
reonsu <manaog.lorenz25@gmail.com>
2024-06-18 23:45:53 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    <link rel="stylesheet" href="./index.css">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat-BLACK&display=swap" rel="stylesheet">
+    <title>Login</title>
+</head>
+<body background="tech.jpg">
+    <section class="side">
+        <img src="logo_vector.png" alt="">
+    </section>
+
+    <section class="main">
+        <div class="login-container">
+            <p class="title">EMPLOYEE MANAGEMENT SYSTEM</p>
+            <div class="separator"></div>
+            <p class="welcome-message">Please, provide login credential to proceed and have access to all our services</p>
+
+            <form class="login-form">
+                <div class="form-control">
+                    <input type="text" placeholder="Email">
+                    <i class="fas fa-user"></i>
+                </div>
+                <div class="form-control">
+                    <input type="password" placeholder="Password">
+                    <i class="fas fa-lock"></i>
+                </div>
+
+                <button class="submit">Login</button>
+            </form>
+        </div>
+    </section>
+    
+</body>
+</html>
